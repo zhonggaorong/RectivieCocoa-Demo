e68b0997fb7ea7a494271111a8ff7db0e29cdbf6
zhonggaorong <458673541@qq.com>
2016-05-12 09:29:48 +0800

    Initial Commit

---
+++ RectivieCocoa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RectivieCocoa
+//
+//  Created by 张国荣 on 16/5/12.
+//  Copyright © 2016年 BateOrganization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RectivieCocoa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RectivieCocoa
+//
+//  Created by 张国荣 on 16/5/12.
+//  Copyright © 2016年 BateOrganization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
